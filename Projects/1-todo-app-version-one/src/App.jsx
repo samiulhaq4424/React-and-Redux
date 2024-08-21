53c3ad3983ccb3a0d7c404d47b71900c09243e80
samiulhaq4424 <samiulhaq54424@gmail.com>
2024-08-21 21:29:15 +0530

    second commit

--- Projects/1-todo-app-version-one/src/App.jsx
+++ Projects/1-todo-app-version-one/src/App.jsx
@@ -1,9 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
 import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
 import TodoItem1 from "./components/TodoItem1";
 import TodoItem2 from "./components/TodoItem2";
-import "./App.css";
 
 function App() {
   //don't write "class", write "className"
